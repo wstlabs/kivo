ad6029081835479f2998f755b845ae35a50984d6
wstlabs <notyet@notyet.org>
2017-11-19 11:38:02 -0500

    better 'resolve' mechanism

--- kivo/command/__init__.py
+++ kivo/command/__init__.py
@@ -7,21 +7,23 @@ from ..special import match
 
 
 def resolve(command):
+    """
+    Given a (presumably valid) command name, attempts to resolve to the appropriate
+    handler for the module corresponding to that command, and returns it.  If the module
+    isn't loaded (or the command is syntactically invalid), returns None.
+    """
     funcname = "%s.perform" % command
-    if not is_legit(command):
-        raise ValueError("invalid command: %s" % funcname)
-    try:
-        handler = eval(funcname)
-        return handler
-    except Exception as e:
-        raise ValueError("invalid command: %s" % funcname)
+    if is_legit(command):
+        try:
+            handler = eval(funcname)
+            return handler
+        except Exception as e:
+            pass
+    return False
 
-
-
-_pat = re.compile('^\w+$')
+_pat = re.compile('^[_\w]+$')
 def is_legit(command):
     return bool(re.match(_pat,command))
-
 
 def nope():
     """
