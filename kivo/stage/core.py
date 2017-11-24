2fb27295ec407e9e030471140fbfba9e9003e0a2
wstlabs <notyet@notyet.org>
2017-11-24 14:29:06 -0500

    indent functions

--- kivo/stage/core.py
+++ kivo/stage/core.py
@@ -13,35 +13,35 @@ class Stage(object):
         self.rootdir = rootdir
 
 
-def dirpath(phase,prefix,stage=STAGE):
-    j = PHASERANK.get(phase)
-    phasedir = "%d-%s" % (j,phase) if j is not None else phase
-    return "%s/%s/%s" % (stage,phasedir,prefix)
+    def dirpath(phase,prefix,stage=STAGE):
+        j = PHASERANK.get(phase)
+        phasedir = "%d-%s" % (j,phase) if j is not None else phase
+        return "%s/%s/%s" % (stage,phasedir,prefix)
 
-def filepath(phase,prefix,name,stage=STAGE):
-    _dirpath = dirpath(phase,prefix,stage)
-    return "%s/%s.csv" % (_dirpath,name)
+    def filepath(phase,prefix,name,stage=STAGE):
+        _dirpath = dirpath(phase,prefix,stage)
+        return "%s/%s.csv" % (_dirpath,name)
 
-def mkdir_phase(phase,prefix,stage=STAGE,autoviv=False):
-    _dirpath = dirpath(phase,prefix,stage)
-    if not os.path.exists(_dirpath):
-        if autoviv:
-            os.mkdir(_dirpath)
-        else:
-            raise ValueError("invalid state -- can't find dirpath '%s'" % _dirpath)
-    return _dirpath
+    def mkdir_phase(phase,prefix,stage=STAGE,autoviv=False):
+        _dirpath = dirpath(phase,prefix,stage)
+        if not os.path.exists(_dirpath):
+            if autoviv:
+                os.mkdir(_dirpath)
+            else:
+                raise ValueError("invalid state -- can't find dirpath '%s'" % _dirpath)
+        return _dirpath
 
-def mkpath(phase,prefix,name,stage=STAGE,autoviv=False):
-    _dirpath = mkdir_phase(phase,prefix,stage,autoviv)
-    return "%s/%s.csv" % (_dirpath,name)
+    def mkpath(phase,prefix,name,stage=STAGE,autoviv=False):
+        _dirpath = mkdir_phase(phase,prefix,stage,autoviv)
+        return "%s/%s.csv" % (_dirpath,name)
 
-def latest(prefix,name,stage=STAGE):
-    for phase in PHASERANK.keys():
-        _filepath = filepath(phase,prefix,name,stage)
-        # print("%s.%s:%s -> %s" % (prefix,name,phase,_filepath))
-        if os.path.exists(_filepath):
-            return _filepath
-    return None
+    def latest(prefix,name,stage=STAGE):
+        for phase in PHASERANK.keys():
+            _filepath = filepath(phase,prefix,name,stage)
+            # print("%s.%s:%s -> %s" % (prefix,name,phase,_filepath))
+            if os.path.exists(_filepath):
+                return _filepath
+        return None
 
 
 """
