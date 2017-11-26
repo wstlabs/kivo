c8f74b256c35b3dcc10bec1d88cfdddfa38d8f9d
wstlabs <notyet@notyet.org>
2017-11-26 11:48:17 -0500

    list command - stub return

--- kivo/command/list_.py
+++ kivo/command/list_.py
@@ -4,6 +4,7 @@ from ..decorators import timedsingle
 from .. import source
 from .. import stage
 from .util.execute import exec_noarg
+from .. import module
 
 def perform(posargs=None,options=None):
     log.debug("posargs=%s, options=%s" % (posargs,options))
@@ -12,5 +13,11 @@ def perform(posargs=None,options=None):
 @timedsingle
 def list_modules(options):
     log.debug('yo!')
+    modules = module.find()
+    _pl = 's' if len(modules) else ''
+    print("Found %d module%s:" % (len(modules),_pl))
+    for name in modules:
+        print(f' - {name}')
+    log.debug('there.')
     return True
 
