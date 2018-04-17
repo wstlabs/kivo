dbbbd4440eb54e6e8eb559784449ce793f81e102
wstlabs <notyet@notyet.org>
2018-04-17 12:34:52 -0400

    prune cruft

--- kivo/command/refresh.py
+++ kivo/command/refresh.py
@@ -20,82 +20,9 @@ def refresh_matview(name):
     log.debug("psql = [%s]" % psql)
     return dopsql(psql,kivo.pgconf)
 
-# HANDLERS = {
-#   'table':load_table
-# }
-
-# DEPRECATED
-"""
-@timedsingle
-def load_source_canon(prefix,name):
-    _stage = theStage
-    log.debug(f'stage = {_stage}')
-    log.debug(f'source = {prefix}.{name}')
-    infile = _stage.latest(prefix,name)
-    log.info("infile = '%s'" % infile)
-    assert_loadable(prefix,name,infile)
-    # if not permit_loadable(prefix,name):
-    #    raise ValueError("source inactive by configuration")
-    table = tablename('t0',prefix,name)
-    log.info("table = '%s'" % table)
-    psql = make_copy_command(table,infile)
-    log.debug("psql = [%s]" % psql)
-    return dopsql(psql,kivo.pgconf)
-"""
-
-"""
-def permit_loadable(prefix,name):
-    ---A simpe abstracted perms check which allows us to override config settings
-    for certain special sources.---
-    if prefix in ('temp','norm'):
-        return True
-    return source.getval(prefix,name,'active')
-"""
-
 def assert_loadable(tablename,infile):
     if infile is None:
         raise RuntimeError("no loadable file for tablename ='%s'" % (tablename))
     if not os.path.exists(infile):
         raise RuntimeError("can't find infile '%s'" % infile)
-
-def __assert_loadable(prefix,name,infile):
-    if infile is None:
-        raise RuntimeError("no loadable file for prefix = '%s', name ='%s'" % (prefix,name))
-    if not os.path.exists(infile):
-        raise RuntimeError("can't find infile '%s'" % infile)
-
-
-#
-# DEPRECATED
-#
-
-def __load_any(srcarg,strict=True):
-    if '.' in srcarg:
-        srcpath = srcarg
-        prefix,name = splitpath(srcpath)
-        return load_multi(prefix,[name],strict)
-    else:
-        prefix = srcarg
-        names = source.select(prefix,{'active':True})
-        return load_multi(prefix,names,strict)
-
-def __load_multi(prefix,names,strict=True):
-    """Load multiple named sourcs under a given prefix."""
-    log.debug("names = %s" % names)
-    for name in names:
-        log.info("source %s.%s .." % (prefix,name))
-        status,delta = load_source_named(prefix,name)
-        _status = 'OK' if status else 'FAIL'
-        log.info("source %s.%s - status = %s in %.3f sec" % (prefix,name,_status,delta))
-        if strict and not status:
-            return False
-    return True
-
-def __perform(posargs=None,options=None):
-    log.debug("posargs=%s, options=%s" % (posargs,options))
-    if len(posargs) == 1:
-        return exec_source(load_source_named,posargs)
-    else:
-        raise ValueError("invalid usage")
-
 
