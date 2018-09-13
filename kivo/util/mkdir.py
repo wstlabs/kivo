66fdfdffef9c16543488d9c33ac87a7cbe01e091
wstlabs <notyet@notyet.org>
2018-09-13 13:57:34 +0200

    tidy function innards

--- kivo/util/mkdir.py
+++ kivo/util/mkdir.py
@@ -1,16 +1,16 @@
 import os
 from .path import splitfull
 
-def mkdir_from_base(basedir,path):
+def mkdir_from_base(basedir,subpath):
     if not os.path.isdir(basedir):
         raise ValueError(f"can't find basedir '{basedir}'")
-    path = splitfull(path)
+    path = splitfull(subpath)
     curpath = basedir
-    print(f"path = {path}")
+    # print(f"subpath = {subpath}")
     for term in path:
         if '/' in term:
-            raise ValueError(f"can't deal with path terms containing forward slash in term '{term}'")
-        curpath = f"{curpath}/{term}"
+            raise ValueError(f"can't deal with path containing forward slash in term '{term}'")
+        curpath = os.join(curpath,term):
         if not os.path.exists(curpath):
             os.mkdir(curpath)
     return curpath
